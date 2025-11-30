d342911c01c04a9d54c3215058309ebe282f098c
fabricio2fb <162474970+fabricio2fb@users.noreply.github.com>
2025-11-30 01:15:47 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import './globals.css';
 import { cn } from '@/lib/utils';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  verification: {
+    google: '0s706fwe8pqZQt5Ugk7vjz4hAmGWzn9tWOcIuaKMaKk',
+  },
+};
 
 export default function RootLayout({
   children,
